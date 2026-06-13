ccff48568b7206adf4b4a900f4266f97c097a884
Rubén Chavarría <ruben.chavarria@saicone.com>
2026-06-13 17:07:36 -0600

    perf: disable spawner particle task

--- src/main/java/com/songoda/epicspawners/EpicSpawners.java
+++ src/main/java/com/songoda/epicspawners/EpicSpawners.java
@@ -41,7 +41,6 @@ import com.songoda.epicspawners.spawners.spawner.PlacedSpawner;
 import com.songoda.epicspawners.spawners.spawner.SpawnerData;
 import com.songoda.epicspawners.spawners.spawner.SpawnerManager;
 import com.songoda.epicspawners.tasks.AppearanceTask;
-import com.songoda.epicspawners.tasks.SpawnerParticleTask;
 import com.songoda.epicspawners.tasks.SpawnerSpawnTask;
 import org.bukkit.Bukkit;
 import org.bukkit.Material;
@@ -72,7 +71,7 @@ public class EpicSpawners extends SongodaPlugin {
     private BlacklistHandler blacklistHandler;
 
     private AppearanceTask appearanceTask;
-    private SpawnerParticleTask particleTask;
+    //private SpawnerParticleTask particleTask;
     private SpawnerSpawnTask spawnerCustomSpawnTask;
 
     private DatabaseConnector databaseConnector;
@@ -92,7 +91,7 @@ public class EpicSpawners extends SongodaPlugin {
     public void onPluginDisable() {
         if (!spawnerManager.wasConfigModified())
             this.saveToFile();
-        this.particleTask.cancel();
+        //this.particleTask.cancel();
         this.spawnerCustomSpawnTask.cancel();
         this.databaseConnector.closeConnection();
         HologramManager.removeAllHolograms();
@@ -158,7 +157,7 @@ public class EpicSpawners extends SongodaPlugin {
         Bukkit.getScheduler().runTaskTimerAsynchronously(this, this::saveToFile, timeout, timeout);
 
         // Start tasks
-        this.particleTask = SpawnerParticleTask.startTask(this);
+        //this.particleTask = SpawnerParticleTask.startTask(this);
         this.spawnerCustomSpawnTask = SpawnerSpawnTask.startTask(this);
         this.appearanceTask = AppearanceTask.startTask(this);
 
